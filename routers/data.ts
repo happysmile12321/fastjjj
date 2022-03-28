73e5b5409314dc844935763e1dd9ce7ed3f2898c
happysmile <812024380@qq.com>
2022-03-28 11:29:05 +0800

    add

--- routers/data.ts
+++ routers/data.ts
@@ -2,5 +2,6 @@ export var books = [
     {id:0,name:"sure"},
     { id: 1, name: 'zhangsan' },
   { id: 2, name: 'lisi' },
-    {id:3,name:"hahhaha"}
+  { id: 3, name: "hahhaha" },
+    {id:99,name:'yes'}
   ]
